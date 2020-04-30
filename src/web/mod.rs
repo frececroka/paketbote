9ac758177c5da85411fbb47fbeeac50200db5481
Lorenz Gorse <lorenz.gorse@gmail.com>
2020-04-30 21:25:40 +0200

    Add catcher for HTTP 409.

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -5,9 +5,16 @@ mod routes;
 
 use db::Db;
 
+#[catch(409)]
+fn catch_409_conflict() -> String {
+    "This package/version/arch combination already exists.\n".into()
+}
+
 pub fn run() {
     rocket::ignite()
         .attach(Db::fairing())
+        .register(catchers![
+            catch_409_conflict])
         .mount("/", routes![
             routes::upload::upload,
             routes::getfile::getfile])
