feef0d13acdd525f73346cb475cd17d722c7c331
Lorenz Gorse <lorenz.gorse@gmail.com>
2020-05-16 20:19:35 +0200

    Cool new stuff.

--- src/web/routes/account.rs
+++ src/web/routes/account.rs
@@ -3,8 +3,8 @@ use rocket::http::Status;
 use rocket_contrib::templates::Template;
 use serde::Serialize;
 
-use crate::db::{get_account_by_name, get_repos_by_account};
-use crate::db::models::{Account, Repo};
+use crate::db::{get_account_by_name, get_packages_by_repo, get_repos_by_account, models};
+use crate::db::models::{Account, Package};
 use crate::web::ctx_base::BaseContext;
 use crate::web::props::Props;
 
@@ -13,6 +13,22 @@ struct AccountContext {
     base: BaseContext,
     account: String,
     repos: Vec<Repo>
+}
+
+#[derive(Serialize)]
+struct Repo {
+    name: String,
+    packages: usize,
+    byte_size: usize
+}
+
+impl Repo {
+    fn new(repo: models::Repo, packages: Vec<Package>) -> Repo {
+        let name = repo.name;
+        let byte_size = packages.iter().map(|p| p.size as usize).sum();
+        let packages = packages.len();
+        Repo { name, packages, byte_size }
+    }
 }
 
 impl AccountContext {
@@ -30,7 +46,14 @@ pub fn route_account(props: Props, account: String) -> Template {
         .map_err(|_| Status::InternalServerError)?
         .ok_or(Status::NotFound)?;
     let repos = get_repos_by_account(&*props.db, account.id)
-        .map_err(|_| Status::InternalServerError)?;
+        .map_err(|_| Status::InternalServerError)?
+        .into_iter()
+        .map(|repo| {
+            let packages = get_packages_by_repo(&*props.db, repo.id)
+                .map_err(|_| Status::InternalServerError)?;
+            Ok(Repo::new(repo, packages))
+        })
+        .collect::<Result<Vec<_>, _>>()?;
     let context = AccountContext::new(&props, account, repos);
     Template::render("account", context)
 }
